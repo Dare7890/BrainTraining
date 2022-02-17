bb7342a1333934b38c0c92e9ee0b7e7fe8013b19
Fetisov Vladislav <vladfetisov2011@yandex.ru>
2022-02-17 13:03:55 +0300

    add show question info on site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 'use strict'
 
 import { getQuestions } from './questionRetriever.js';
+
+function start(){
+    let questions = getQuestions();
+    showQuestion(questions[0]);
+}
+
+function showQuestion(question){
+    let questionElement = document.getElementsByClassName("question")[0];
+    let questionNumber = document.createElement("p");
+    questionNumber.classList.add("question-number");
+    questionNumber.textContent = `Вопрос №${question.number}`;
+    
+    let questionText = document.createElement("p");
+    questionText.classList.add("question-text");
+    questionText.textContent = question.expression;
+
+    questionElement.append(questionNumber);
+    questionElement.append(questionText);
+}
+
+start();
